036ee9b7824dc5295a82bb197866b21dec176c22
Sirfame Lin <sirfame@hotmail.com>
2018-12-09 20:26:09 -0800

    adding react tutorial

---
+++ build/precache-manifest.6f6ddd6d08b431fe4ad8ad7967ed7fc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2790d252b16d03c8c226",
+    "url": "/static/css/main.760d312a.chunk.css"
+  },
+  {
+    "revision": "2790d252b16d03c8c226",
+    "url": "/static/js/main.2790d252.chunk.js"
+  },
+  {
+    "revision": "5bc363a705c279ce0d67",
+    "url": "/static/js/1.5bc363a7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a9dff57b782252ca9614e9417328d58b",
+    "url": "/index.html"
+  }
+];
